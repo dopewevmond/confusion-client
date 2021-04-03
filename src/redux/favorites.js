5f36383f75352690a84749e0322355297e57fbaa
dopewevmond <68214494+dopewevmond@users.noreply.github.com>
2021-04-03 03:44:41 +0000

    Fix state to prevent error, fixed logo url

--- src/redux/favorites.js
+++ src/redux/favorites.js
@@ -3,14 +3,16 @@ import * as ActionTypes from './ActionTypes';
 export const favorites = (state = {
         isLoading: true,
         errMess: null,
-        favorites: null
+        favorites: {
+            dishes: []
+        }
     }, action) => {
     switch(action.type) {
         case ActionTypes.ADD_FAVORITES:
             return {...state, isLoading: false, errMess: null, favorites: action.payload};
 
         case ActionTypes.FAVORITES_LOADING:
-            return {...state, isLoading: true, errMess: null, favorites: null};
+            return {...state, isLoading: true, errMess: null, favorites: { dishes: []}};
 
         case ActionTypes.FAVORITES_FAILED:
             return {...state, isLoading: false, errMess: action.payload, favorites: null};
